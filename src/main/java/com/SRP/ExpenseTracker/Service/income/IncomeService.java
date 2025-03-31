6bcbe2d0095b2c06493fd222f147c2642545ba3a
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-31 23:45:05 +0530

    made changes in the Income  Service

--- src/main/java/com/SRP/ExpenseTracker/Service/income/IncomeService.java
+++ src/main/java/com/SRP/ExpenseTracker/Service/income/IncomeService.java
@@ -8,8 +8,13 @@ import com.SRP.ExpenseTracker.entity.Income;
 public interface IncomeService {
 
 	Income postIncome(IncomeDTO incomeDTO);
+
 	List<IncomeDTO> getAllIncomes();
+
     Income updateIncome(Long id, IncomeDTO incomeDTO);
+
     IncomeDTO getIncomeById(Long id);
+
     void deleteIncome(Long id);
+
 }
